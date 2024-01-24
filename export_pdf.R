598785a62e40a106b174b7d662bc9a85341be84a
lemuelemos <lemuelemos@gmail.com>
2024-01-24 08:14:45 -0300

    Pequenos ajustes para export de pdf

--- export_pdf.R
+++ export_pdf.R
@@ -1,1 +1,5 @@
-pagedown::chrome_print("index.html")
+pagedown::chrome_print("index.html", "index.pdf",format = "pdf")
+pdftools::pdf_subset("index.pdf",pages = 1,output = "Curriculo_Lemuel.pdf")
+file.remove("index.pdf")
+
+
